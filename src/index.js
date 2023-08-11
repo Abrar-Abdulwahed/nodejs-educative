687774038fb7de4507e214a3e841a1a332f0dae9
A-Abd <abrar.abdulwahed@gmail.com>
2023-08-11 23:51:28 +0300

    feat: adding middleware

--- src/index.js
+++ src/index.js
@@ -3,15 +3,23 @@ const path = require('path');
 
 const app = express();
 
+app.use((req, res, next) => {
+  const { method, path } = req;
+  console.log(
+    `New request to: ${method} ${path} at ${new Date().toISOString()}`
+  );
+  next();
+});
+
 const publicDirectoryPath = path.join(__dirname, './public');
 app.use(express.static(publicDirectoryPath));
 
 app.get("/", (req, res) => {
- res.send("Hello Express Student!");
+res.send("Hello Express Student!");
 });
 
 app.get("/:name", (req, res) => {
-  res.send(`Welcome to Express Recipes, ${req.params.name}!`);
+ res.send(`Welcome to Express Recipes, ${req.params.name}!`);
 });
 
 const port = process.env.PORT || 8080;
