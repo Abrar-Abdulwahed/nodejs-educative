ac6704d639093dc73329794d3a9d3b381db499ce
A-Abd <abrar.abdulwahed@gmail.com>
2023-08-11 23:59:13 +0300

    feat: adding api endpoint

--- src/index.js
+++ src/index.js
@@ -1,29 +1,22 @@
 const express = require("express");
 const path = require('path');
 
+const recipesRouter = require('./routers/recipes');
+
 const app = express();
 
 app.use((req, res, next) => {
   const { method, path } = req;
   console.log(
-    `New request to: ${method} ${path} at ${new Date().toISOString()}`
+  `New request to: ${method} ${path} at ${new Date().toISOString()}`
   );
   next();
 });
 
-const publicDirectoryPath = path.join(__dirname, './public');
-app.use(express.static(publicDirectoryPath));
-
-app.get("/", (req, res) => {
-res.send("Hello Express Student!");
-});
-
-app.get("/:name", (req, res) => {
- res.send(`Welcome to Express Recipes, ${req.params.name}!`);
-});
+app.use('/api/v1/recipes', recipesRouter);
 
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
- console.log(`Server is up on port ${port}.`);
+  console.log(`Server is up on port ${port}.`);
 });
