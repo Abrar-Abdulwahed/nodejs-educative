62ce58e7aa35064b988414a25eab4601ea15678c
A-Abd <abrar.abdulwahed@gmail.com>
2023-08-12 19:52:53 +0300

    feat: build structure of restful api with express

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
+// const express = require("express");
+// const path = require('path');
+
+// const app = express();
+
+// // Define the route handler for the "/hello" endpoint
+// app.get('/hello', (req, res) => {
+//   const greeting = { message: 'Hello, world!' };
+//   res.json(greeting);
+// });
+
+
+
+// const port = process.env.PORT || 8080;
+
+// app.listen(port, () => {
+//  console.log(`Server is up on port ${port}.`);
+// });
+
 const express = require("express");
 const path = require('path');
 
